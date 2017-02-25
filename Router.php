d1d9ec1635339f0b102dcc6b74bb21675755a470
Zachary DuBois <me@zacharydubois.me>
2017-02-25 12:52:12 -0500

    Add some base.

---
+++ Router.php
@@ -0,0 +1,66 @@
+<?php
+
+class Router {
+    /**
+     * Process URI
+     * Processes the current URIs with the given array.
+     *
+     * @param array $routes Array of routes for different URIs
+     *
+     * @return bool
+     */
+    public static function process(array $routes) {
+        $requestURI = self::preProcess();
+
+        if (self::checkURI() === false) {
+            return true;
+        }
+
+        foreach ($routes as $route) {
+            if ($route['method'] === strtolower(filter_input(INPUT_SERVER, 'REQUEST_METHOD'))) {
+                $matched = preg_match('/^' . $route['uri'] . '$/', $requestURI, $matches);
+
+                if ($matched === 1) {
+                    call_user_func($route['func'], $matches);
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Pre-Process
+     * Pre-processes the REQUEST_URI for use with static::process.
+     *
+     * @return string Processed URI
+     */
+    private static function preProcess() {
+        $requestURI = substr(filter_input(INPUT_SERVER, 'REQUEST_URI'), strlen(MOTDBASE));
+        $requestURI = strtok($requestURI, '?');
+
+        return $requestURI;
+    }
+
+    /**
+     * Remove Slash
+     * Removes the trailing slash of a URL and redirects the user.
+     *
+     * @return bool False if check fails.
+     */
+    private static function checkURI() {
+        $uri = filter_input(INPUT_SERVER, 'REQUEST_URI');
+
+        if (preg_match('/^(.+)(\/)(|\?.+)$/', $uri, $matches) === 1) {
+            $uri = $matches[1] . $matches[3];
+
+            header('Location: ' . $uri);
+
+            return false;
+        }
+
+        return true;
+    }
+}
