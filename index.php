f9f481133cd5c67f10bbeff9654c6a77408f4df5
EthanHeywood <Ethanh@frontier.com>
2017-02-25 13:52:04 -0500

    Adds simple routing table

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+include_once __DIR__ . DIRECTORY_SEPARATOR . 'Router.php';
+function matchScouting($param){
+
+}
+
+$routingArray = array(
+  //returns scouting form for
+  array(
+    'method' => 'get',
+    'func' => 'matchScouting',
+    'uri' => 'match\/([0-9]{1,})'
+  )
+
+);
