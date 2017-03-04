38b8fca7755848222a6e83a7d21efcda88ee84ab
Zachary DuBois <me@zacharydubois.me>
2017-03-04 00:02:37 -0500

    Make the ExtractorClient smarter.

--- ExtractorClient.php
+++ ExtractorClient.php
@@ -1,59 +1,82 @@
 <?php
 
 /**
-  *returns config
-  *
-  *handles various tasks, loads config
-  *
-  *
-  *
-  *
-  */
-class ExtractorClient{
-  private $config;
-  public function __construct(){
-    $this->loadConfig();
-  }
+ * Class ExtractorClient
+ */
+class ExtractorClient {
+    private $config;
 
-/**
-  *loadConfig
-  *
-  * reloads data in config
-  *
-  * @return true
-  */
-  private function loadConfig(){
-    $this->config = file_get_contents(CONFIG);
-    $this->config = json_decode($this->config, true);
-    return true;
-  }
+    /**
+     * ExtractorClient constructor.
+     * Pre-loads config.
+     */
+    public function __construct() {
+        $this->loadConfig();
+    }
 
-  /**
-    *getConfig
-    * just returns config
-    *
-    * @return config
-    */
-  public function getConfig(){
-    return $this->config;
-  }
+    /**
+     * Load Config
+     * Loads Extractor's configuration file.
+     *
+     * @return true
+     */
+    private function loadConfig() {
+        $this->config = file_get_contents(CONFIG);
+        $this->config = json_decode($this->config, true);
 
-  /**
-    *setConfig
-    *verifys that $config
-    *
-    * @param array $config
-    *
-    * @return false|true
-    */
-  public function setConfig($config){
-      if(!is_array($config)){
+        return true;
+    }
+
+    /**
+     * Save Config
+     * Saves the changes to the configuration files.
+     *
+     * @return true
+     */
+    private function saveConfig() {
+        file_put_contents(CONFIG, json_encode($this->config, JSON_PRETTY_PRINT));
+
+        return true;
+    }
+
+    /**
+     * Get Config
+     * Gets the desired configuration key.
+     *
+     * @param string $key Config key
+     *
+     * @return mixed
+     */
+    public function getConfig($key) {
+        if (array_key_exists($key, $this->config)) {
+            return $this->config[$key];
+        }
+
         return false;
-      }
+    }
 
-      $config = json_encode($config);
-      file_put_contents(CONFIG, $config);
-      $this->loadConfig();
-      return true;
-  }
+    /**
+     * Sets Config Key
+     * Sets the selected configuration key to the desired value.
+     *
+     * @param string $key   Config key
+     * @param mixed  $value Desired config value
+     *
+     * @return true;
+     */
+    public function setConfig($key, $value) {
+        if (!is_string($key)) {
+            return false;
+        }
+
+        if ($value === null && array_key_exists($key, $this->config)) {
+            unset($this->config[$key]);
+        } else {
+            $this->config[$key] = $value;
+        }
+
+        $this->saveConfig();
+
+        return true;
+    }
 }
