703654c87ff349a8361673f870677bfa3ff86ccc
Zachary DuBois <me@zacharydubois.me>
2017-03-04 11:14:42 -0500

    Add ExtractorScouting.

---
+++ inc/ExtractorScouting.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Class ExtractorScouting
+ */
+class ExtractorScouting {
+    private $type, $key, $data;
+
+    /**
+     * ExtractorScouting constructor.
+     * Initializes private variable with current storage contents.
+     *
+     * @param string $type Type of data (match or pit)
+     * @param string $key  Match number or team number.
+     */
+    public function __construct($type, $key) {
+        $this->type = $type;
+        $this->key = $key;
+
+        $fetch = ExtractorStorage::fetch($this->type, $this->key);
+
+        if (!$fetch) {
+            $this->data = array();
+        }
+
+        $this->data = $fetch;
+    }
+
+    /**
+     * Set Data
+     * Pre handler for data before being passed into the storage.
+     *
+     * @param array $data Data array
+     *
+     * @return bool
+     */
+    public function set($data) {
+        if (!is_array($data)) {
+            return false;
+        }
+
+        $this->data = $data;
+
+        return true;
+    }
+
+    /**
+     * Save Data
+     * Saves the data to disk and marks the data as not transferred using ExtractorTransferUtil.
+     *
+     * @return true
+     */
+    public function save() {
+        ExtractorStorage::store($this->type, $this->key, $this->data);
+        ExtractorTransferUtil::setTransferred($this->type, $this->key, false);
+
+        return true;
+    }
+
+    /**
+     * Get Data
+     * Get the data file.
+     *
+     * @return array
+     */
+    public function get() {
+        return $this->data;
+    }
+
+    /** @noinspection PhpUnusedPrivateMethodInspection */
+    /**
+     * TODO: Validate data array.
+     */
+    private function validate() {
+
+    }
+}
