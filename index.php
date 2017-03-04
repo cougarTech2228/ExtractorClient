4c81bcbc6762b05dd1afe924f4c2fbfe4ca546a4
Zachary DuBois <me@zacharydubois.me>
2017-03-03 19:47:37 -0500

    Properly format.

--- index.php
+++ index.php
@@ -47,28 +47,31 @@ function config($param) {
 function postConfig($param) {
 
 }
+
 /**
-* return404
-* returns browser to a 404 page
-*
-* @return string|false
-*/
+ * return404
+ * returns browser to a 404 page
+ *
+ * @return string|false
+ */
 function return404() {
-  header('HTTP/1.0 404 Not Found');
-  return render('404', array());
+    header('HTTP/1.0 404 Not Found');
+
+    return render('404', array());
 }
 
 /**
-* redirect
-* redirects browser to a different location
-*
-* @param string $uri redirect to
-*
-* @return true
-*/
+ * redirect
+ * redirects browser to a different location
+ *
+ * @param string $uri redirect to
+ *
+ * @return true
+ */
 function redirect($uri) {
-  header('Location: '.$uri);
-  return true;
+    header('Location: ' . $uri);
+
+    return true;
 }
 
 /**
@@ -76,24 +79,25 @@ function redirect($uri) {
  * Renders a Template in Mustache
  *
  * @param string $template template file
- * @param array $context gives context to template
+ * @param array  $context  gives context to template
  *
  * @return false|string updated php dock
  */
-function render($template, $context){
-  if(!isset($template) || isset($context)){
-    return false;
-  }
+function render($template, $context) {
+    if (!isset($template) || isset($context)) {
+        return false;
+    }
 
-  if(!file_exists('templates/partial/'.$template.'.mustache')){
-    return false;
-  }
-  $mustache = new Mustache_Engine(array(
-    'loader' => new Mustache_Loader_FilesystemLoader('templates/'),
-    'partials_loader' => new Mustache_Loader_FilesystemLoader('templates/partial')
-  ));
-  $render = $mustache->loadTemplate($template);
-  return $render->render($context);
+    if (!file_exists('templates/partial/' . $template . '.mustache')) {
+        return false;
+    }
+    $mustache = new Mustache_Engine(array(
+        'loader'          => new Mustache_Loader_FilesystemLoader('templates/'),
+        'partials_loader' => new Mustache_Loader_FilesystemLoader('templates/partial')
+    ));
+    $render = $mustache->loadTemplate($template);
+
+    return $render->render($context);
 }
 
 $routingArray = array(
