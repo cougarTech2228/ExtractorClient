c3d50783f9ce858a701304fe057456a734b0479b
EthanHeywood <Ethanh@frontier.com>
2017-03-03 20:10:17 -0500

    Added documentation

--- ExtractorClient.php
+++ ExtractorClient.php
@@ -1,21 +1,51 @@
 <?php
 
+/**
+  *returns config
+  *
+  *handles various tasks, loads config
+  *
+  *
+  *
+  *
+  */
 class ExtractorClient{
   private $config;
   public function __construct(){
     $this->loadConfig();
   }
 
+/**
+  *loadConfig
+  *
+  * reloads data in config
+  *
+  * @return true
+  */
   private function loadConfig(){
     $this->config = file_get_contents(CONFIG);
     $this->config = json_decode($this->config, true);
     return true;
   }
 
+  /**
+    *getConfig
+    * just returns config
+    *
+    * @return config
+    */
   public function getConfig(){
     return $this->config;
   }
 
+  /**
+    *setConfig
+    *verifys that $config
+    *
+    * @param array $config
+    *
+    * @return false|true
+    */
   public function setConfig($config){
       if(!is_array($config)){
         return false;
@@ -26,5 +56,4 @@ class ExtractorClient{
       $this->loadConfig();
       return true;
   }
-
 }
